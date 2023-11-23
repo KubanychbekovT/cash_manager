43afb3c59fa0f83b4e9f64a0e982804ecb02e489
Temirlan <brucewaynetemir@gmail.com>
2023-11-23 21:34:27 +0600

    refactor: income_page

--- lib/presentation/transaction/income/income_page.dart
+++ lib/presentation/transaction/income/income_page.dart
@@ -3,6 +3,7 @@ import 'package:cash_manager/application/transaction/transaction_watcher/transac
 import 'package:cash_manager/domain/transaction/income/income.dart';
 import 'package:cash_manager/injection.dart';
 import 'package:cash_manager/presentation/core/widgets/failure_snackbar.dart';
+import 'package:cash_manager/presentation/transaction/widgets/amount_text_field.dart';
 import 'package:cash_manager/presentation/transaction/widgets/name_text_field.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
@@ -85,13 +86,113 @@ class IncomePage extends StatelessWidget {
                       },
                     ),
                     SizedBox(height: 20),
-
+                    BlocBuilder<IncomeFormCubit, IncomeFormState>(
+                      builder: (context, state) {
+                        return AmountTextField(
+                          state: right(state),
+                          controller: _amountController,
+                          onChanged: (String value) {
+                            context
+                                .read<IncomeFormCubit>()
+                                .amountChanged(value);
+                          },
+                        );
+                      },
+                    ),
+                    SizedBox(height: 4),
+                    Row(
+                      children: [
+                        templatePriceWidget(10, context),
+                        templatePriceWidget(50, context),
+                        templatePriceWidget(100, context),
+                        templatePriceWidget(500, context),
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    Row(
+                      children: [
+                        Visibility(
+                          visible: income != null,
+                          child: Expanded(
+                              child: SizedBox(
+                            height: 40,
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.red),
+                              onPressed: () {
+                                context
+                                    .read<IncomeFormCubit>()
+                                    .deleteIncome(income!);
+                              },
+                              child: Text(
+                                "Delete",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                            ),
+                          )),
+                        ),
+                        Visibility(
+                          visible: income != null,
+                          child: SizedBox(width: 10),
+                        ),
+                        Expanded(
+                            child: SizedBox(
+                          height: 40,
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xff00b6e4)),
+                            onPressed: () {
+                              if (income == null) {
+                                context.read<IncomeFormCubit>().createIncome();
+                              } else {
+                                context
+                                    .read<IncomeFormCubit>()
+                                    .updateIncome(income!);
+                              }
+                            },
+                            child: Text(
+                              income != null ? "Save" : "Add",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                        ))
+                      ],
+                    ),
+                    SizedBox(height: 20),
                   ],
                 ),
               ),
             );
           },
         ),
+      ),
+    );
+  }
+
+  Widget templatePriceWidget(int amount, BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.read<IncomeFormCubit>().amountChanged(amount.toString());
+        _amountController.text = amount.toString();
+      },
+      child: BlocBuilder<IncomeFormCubit, IncomeFormState>(
+        builder: (context, state) {
+          return Container(
+            margin: EdgeInsets.only(right: 10),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(50)),
+            child: Text(
+              '\$ $amount'.toUpperCase(),
+              style: TextStyle(fontSize: 12, color: Colors.grey[900]),
+            ),
+          );
+        },
       ),
     );
   }
