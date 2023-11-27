95297746acd0cc81db57bc260eb185e16b8f7473
Temirlan <brucewaynetemir@gmail.com>
2023-11-27 16:13:11 +0600

    feat: add constants

--- lib/presentation/core/constants.dart
+++ lib/presentation/core/constants.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
 
 class PageRoutes{
-  static const homePage = '/home';
-  static const profilePage = 'profile_page';
+  static const transactionPage="/transaction";
+  static const detailPage="detailPage";
+  static const profilePage="profile_page";
 }
 class AppColorConstants{
-  static const lightScaffoldBackgroundColor=Color(0xff0039a5);
+  static const lightScaffoldBackgroundColor=Colors.white;
   static const darkScaffoldBackgroundColor=Color(0xFF1C7549);
   static const lightPrimaryColor=Color(0xff74D25D);
   static const darkPrimaryColor=Colors.green;
 }
+class AppConstants{
+  static const myServer="127.0.0.1:8000";
+  static const fcmServer="https://fcm.googleapis.com/fcm/send";
+  static const fcmApiToken="AAAAE0e1ZXU:APA91bEBCR6NqGsX2J7pVZO4mQW8x6kYHL5GragMbBmjIdXtxQ6d5xqdiQUzJ8Ho5eu_1-YIZUpzS7yH9_qHnXmJUWOrmK96SJBqq0xpBpm5nO-4pNjpaISKgzk7luYzYYttnCcg6qN6";
+  static const isDark="is_dark";
+  static const locale="locale";
+}
+List<String> chartMonths = [
+  "Jan",
+  "",
+  "Mar",
+  "",
+  "May",
+  "",
+  "Jul",
+  "",
+  "Sep",
+  "",
+  "Nov",
+  ""
+];
+final List<String> weekDays = [
+  "mon",
+  "tue",
+  "wed",
+  "thu",
+  "fri",
+  "sat",
+  "sun",
+];
+final List<String> months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
